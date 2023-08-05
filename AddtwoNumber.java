46b3f802973d6189f8d603b2fe58a2afc8d3f073
Abhishek Kumar <109984370+InfernoTech07@users.noreply.github.com>
2023-08-05 08:35:48 +0530

    Add files via upload

---
+++ AddtwoNumber.java
@@ -0,0 +1,10 @@
+public class AddtwoNumber {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("Sum:"+sum);
+        System.out.println(a+b);
+    }
+    
+}
