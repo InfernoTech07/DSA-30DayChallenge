cf016ab9edd7a97c6867d9bae3bbbd33e3292090
Abhishek Kumar <109984370+InfernoTech07@users.noreply.github.com>
2023-08-05 10:35:45 +0530

    Add files via upload

---
+++ naturalNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class naturalNumber {
+    public static void main(String args[]){
+        int n;
+        System.out.println("Enter Number:");
+        Scanner sc =new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.print(i+" ");
+        }
+
+    }
+    
+}
