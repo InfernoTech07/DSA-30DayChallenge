0bc5eb119739a7d2c19cdd86c264cdfa8b61c1d1
Abhishek Kumar <109984370+InfernoTech07@users.noreply.github.com>
2023-08-06 08:44:00 +0530

    Add files via upload

---
+++ gratestNumberBwTwo.java
@@ -0,0 +1,14 @@
+public class gratestNumberBwTwo {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+
+        if(a>b){
+            System.out.print("a is greater:"+a);
+        }
+        else{
+            System.out.println("b is greater:"+b);
+        }
+    }
+    
+}
