89a2b5cdf4ada866f9c59362383f5cca23ade6ba
Abhishek Kumar <109984370+InfernoTech07@users.noreply.github.com>
2023-08-08 10:08:25 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,77 @@
+class Pattern{
+    public static void main (String args[]){
+         int n=5;
+
+         for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+         }
+
+         //Triangle
+
+         for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("@");
+            }
+            System.out.println();
+         }
+
+         // Number
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+         }
+
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+         }
+
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+         }
+
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+         }
+
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+         }
+
+         //pyramid 
+
+         for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+         }
+    }
+
+
+}
